88296b4644f186994d31cf1c3fdc379f5b19b692
suha alghamdi <150838404+suha2023@users.noreply.github.com>
2024-10-05 22:01:24 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+"use strict";
+console.log("Welcome in TypeScript");
+let x = 5;
+if (x >= 5)
+    console.log("Yes");
+else
+    console.log("NO");
+let n1;
+n1 = 10;
+n1 = "ooo";
+n1 = true;
+let sym2 = Symbol("key");
+let sym3 = Symbol("key");
+//# sourceMappingURL=index.js.map
